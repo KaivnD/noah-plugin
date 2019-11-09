e834723d1050f05fca613d5069823e76e30c45c4
KaivnD <KaivnD@hotmail.com>
2019-11-09 10:16:38 +0800

    init:init

---
+++ NoahPlugin/Load3dm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+
+namespace NoahPlugin
+{
+    public class Load3dm : Command
+    {
+        static Load3dm _instance;
+        public Load3dm()
+        {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the Load3dm command.</summary>
+        public static Load3dm Instance
+        {
+            get { return _instance; }
+        }
+
+        public override string EnglishName
+        {
+            get { return "Load3dm"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            string path = "";
+            Result getArgs = RhinoGet.GetString("加载文件", true, ref path);
+            if (File.Exists(path))
+            {
+                bool alreadyOpen = false;
+                RhinoDoc.Open(path, out alreadyOpen);
+                if (alreadyOpen) RhinoApp.WriteLine(string.Format("已加载 {0}", path));
+            } else RhinoApp.WriteLine(string.Format("{0} 文件不存在", path));
+            return Result.Success;
+        }
+    }
+}
---
+++ NoahPlugin/WsObj.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using WebSocketSharp;
+
+namespace NoahPlugin
+{
+
+    public class WsObject
+    {
+        private WebSocket webSocket;
+
+        public int status;
+
+        public string message;
+
+        private string initMessage;
+
+        public event EventHandler changed;
+
+        public WsObject init(string address, string initMessage)
+        {
+            webSocket = new WebSocket(address);
+            this.initMessage = initMessage;
+            webSocket.WaitTime = new TimeSpan(0, 0, 2);
+            connect();
+            return this;
+        }
+
+        protected virtual void onChanged()
+        {
+            this.changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        private WsObject connect()
+        {
+            webSocket.OnOpen += onOpen;
+            webSocket.OnMessage += onMessage;
+            webSocket.OnError += onError;
+            webSocket.OnClose += onClose;
+            webSocket.Connect();
+            return this;
+        }
+
+        public WsObject disconnect()
+        {
+            webSocket.OnOpen -= onOpen;
+            webSocket.OnMessage -= onMessage;
+            webSocket.OnError -= onError;
+            webSocket.OnClose -= onClose;
+            webSocket.Close();
+            return this;
+        }
+
+        private void onOpen(object sender, EventArgs e)
+        {
+            send(initMessage);
+            status = WsObjectStatus.OPEN;
+            onChanged();
+        }
+
+        private void onError(object sender, ErrorEventArgs e)
+        {
+            status = WsObjectStatus.CLOSE;
+            webSocket = null;
+            onChanged();
+        }
+
+        private void onMessage(object sender, MessageEventArgs e)
+        {
+            status = WsObjectStatus.MESSAGE;
+            if (!e.IsPing)
+            {
+                message = e.Data;
+            }
+            onChanged();
+        }
+
+        private void onClose(object sender, CloseEventArgs e)
+        {
+            status = WsObjectStatus.CLOSE;
+            webSocket = null;
+            onChanged();
+        }
+
+        public void send(string msg)
+        {
+            if (webSocket != null && webSocket.IsAlive)
+            {
+                webSocket.Send(msg);
+            }
+        }
+    }
+
+    internal class WsObjectStatus
+    {
+        public static int ERROR = 0;
+
+        public static int OPEN = 1;
+
+        public static int MESSAGE = 2;
+
+        public static int CLOSE = 3;
+
+        public static string GetStatusName(int status)
+        {
+            switch (status)
+            {
+                case 0:
+                    return "Error";
+                case 1:
+                    return "Open";
+                case 2:
+                    return "Message";
+                case 3:
+                    return "Close";
+                default:
+                    return "UNKOWN";
+            }
+        }
+    }
+}

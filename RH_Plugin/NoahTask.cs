bff324f0f3dd9a358efe393fa5c2889e7c6e1ce7
KaivnD <KaivnD@hotmail.com>
2019-11-10 18:58:28 +0800

    feat:更新数据到文档/已经运行到task移动至前台

--- RH_Plugin/NoahTask.cs
+++ RH_Plugin/NoahTask.cs
@@ -1,4 +1,5 @@
-﻿using Grasshopper;
+﻿using GH_IO.Serialization;
+using Grasshopper;
 using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Data;
@@ -79,34 +80,56 @@ namespace Noah
                 return;
             }
 
-            SetInput(doc);
-
             activeCanvas.Document = doc;
             activeCanvas.Document.IsModified = false;
             activeCanvas.Refresh();
             doc.NewSolution(false);
+
+            UpdateData();
         }
 
         public void BringToFront()
         {
-            // TODO 将相关Task GH的文档前置
-        }
+            GH_DocumentServer doc_server = Instances.DocumentServer;
 
-        private void SetInput(GH_Document doc)
-        {
-            GH_ClusterInputHook[] hooks = doc.ClusterInputHooks();
-            GH_Structure<IGH_Goo> data = new GH_Structure<IGH_Goo>();
-            GH_Path path = new GH_Path(0);
-            GH_Number a = new GH_Number(30);
-            data.Append(a);
-            hooks[0].SetPlaceholderData(data);
-            hooks[0].ExpireSolution(true);
+            if (doc_server == null)
+            {
+                ErrorEvent(this, "No Document Server exist!");
+                return;
+            }
+
+            GH_Document doc = doc_server.ToList().Find(x => x.Properties.ProjectFileName == ID.ToString());
+
+            if (doc == null) return;
+
+            GH_Canvas activeCanvas = Instances.ActiveCanvas;
+            if (activeCanvas == null)
+            {
+                ErrorEvent(this, "No Active Canvas exist!");
+                return;
+            }
+
+            activeCanvas.Document = doc;
+            activeCanvas.Refresh();
+            doc.NewSolution(false);
+
+            UpdateData();
         }
 
         internal void SetData(TaskData taskData)
         {
-            // TODO If taskData ID and dataID match some data inside datalist 
-            // then update else add a brand new data to it
+
+            if (!Equals(taskData.ID, ID)) return;
+
+            TaskData match = (from data in dataList
+                              where Equals(data.dataID, taskData.dataID)
+                              select data).FirstOrDefault();
+
+            if (match != null)
+            {
+                match.value = taskData.value;
+                match.name = taskData.name;
+            } else dataList.Add(taskData);
 
             UpdateData();
         }
@@ -115,10 +138,77 @@ namespace Noah
         {
             if (dataList.Count == 0 || dataList == null) return;
 
-            foreach (TaskData data in dataList)
+            GH_DocumentServer doc_server = Instances.DocumentServer;
+
+            if (doc_server == null)
             {
-                // TODO update data to gh document
+                ErrorEvent(this, "No Document Server exist!");
+                return;
             }
+
+            GH_Document doc = doc_server.ToList().Find(x => x.Properties.ProjectFileName == ID.ToString());
+
+            if (doc == null) return;
+
+            var hooks = doc.ClusterInputHooks();
+
+            foreach (var hook in hooks)
+            {
+                TaskData data = dataList.Find(x => x.name == hook.CustomNickName || x.dataID == hook.CustomNickName);
+
+                if (data == null) continue;
+
+                object value = data.value;
+
+                GH_Structure<IGH_Goo> m_data = new GH_Structure<IGH_Goo>();
+
+                GH_Number castNumber = null;
+                GH_String castString = null;
+                if (GH_Convert.ToGHNumber(value, GH_Conversion.Both, ref castNumber))
+                {
+                    m_data.Append(new GH_ObjectWrapper(castNumber));
+                }
+                else if (GH_Convert.ToGHString(value, GH_Conversion.Both, ref castString))
+                {
+                    m_data.Append(new GH_ObjectWrapper(castString));
+                }
+                else
+                {
+                    m_data.Append((IGH_Goo)value);
+                }
+
+                // TODO If value is not change then continue
+
+                hook.SetPlaceholderData(m_data);
+                hook.ExpireSolution(true);
+            }
+        }
+
+        private void StoreOutput()
+        {
+            GH_LooseChunk ghLooseChunk = new GH_LooseChunk("Grasshopper Data");
+            ghLooseChunk.SetGuid("OriginId", this.ID);
+
+            //IGH_Param ghParam = Params.Input[index];
+            //IGH_Structure volatileData = ghParam.VolatileData;
+            //if (string.IsNullOrEmpty(ghParam.NickName))
+            //{
+            //    ErrorEvent(ghLooseChunk, "Parameters without a name will not be included.");
+            //}
+            //else
+            //{
+            //    GH_IWriter chunk = ghLooseChunk.CreateChunk("Block", index);
+            //    chunk.SetString("Name", ghParam.NickName);
+            //    chunk.SetBoolean("Empty", volatileData.IsEmpty);
+            //    if (!volatileData.IsEmpty)
+            //    {
+            //        GH_Structure<IGH_Goo> tree;
+            //        // access.GetDataTree<IGH_Goo>(index, out tree);
+            //        if (!tree.Write(chunk.CreateChunk("Data")))
+            //            ErrorEvent(ghLooseChunk, string.Format("There was a problem writing the {0} data.", (object)ghParam.NickName));
+            //    }
+            //}
+            byte[] bytes = ghLooseChunk.Serialize_Binary();
         }
     }
 

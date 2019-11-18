562cd209310c468524f5b1309272c545cbccd545
KaivnD <KaivnD@hotmail.com>
2019-11-18 19:10:36 +0800

    fix:储存为3dm文件时出现的Bug

--- RH_Plugin/Utils/File3dmWriter.cs
+++ RH_Plugin/Utils/File3dmWriter.cs
@@ -1,5 +1,4 @@
-﻿using Grasshopper.Kernel;
-using Rhino.DocObjects;
+﻿using Rhino.DocObjects;
 using Rhino.FileIO;
 using Rhino.Geometry;
 using System;
@@ -7,8 +6,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Noah.Utils
 {
@@ -16,154 +13,163 @@ namespace Noah.Utils
     {
         public string m_path { set; get; }
         public File3dm m_file { set; get; }
-        public File3dmLayerTable m_layers { set; get; }
+
+        public SortedDictionary<string, Guid> LayerMap;
+        public Dictionary<ObjectAttributes, GeometryBase> ObjectMap;
+
         public File3dmWriter(string path)
         {
-            File3dm _file = null;
+            LayerMap = new SortedDictionary<string, Guid>();
+            ObjectMap = new Dictionary<ObjectAttributes, GeometryBase>();
             if (File.Exists(path))
             {
                 try
                 {
-                    _file = File3dm.Read(path);
-                    _file.Objects.Clear();
-                    _file.AllLayers.Clear();
+                    m_file = File3dm.Read(path);
+                    m_file.Objects.Clear();
+                    m_file.AllLayers.Clear();
                 }
                 catch
                 {
+                    throw new Exception("Cant read this file!");
                 }
             }
             else
             {
-                _file = new File3dm();
+                m_file = new File3dm();
             }
-            m_file = _file;
-            m_layers = _file.AllLayers;
+
             m_path = path;
         }
 
         public void ChildLayerSolution(string name)
         {
-            if (name.Contains("::")) return;
-            string tmp = name.Replace("::", "-");
-            string[] nameArr = tmp.Split('-');
+            if (!name.Contains(Layer.PathSeparator)) return;
+            string tmp = name.Replace(Layer.PathSeparator, "-");
+            string[] arr = tmp.Split('-');
 
-            Layer parent = FindLayerByName(nameArr[0]);
+            Guid parentID;
 
-            if (parent != null) return;
+            if (LayerMap.TryGetValue(arr[0], out parentID)) return;
 
-            Layer l = new Layer
-            {
-                Name = nameArr[0],
-                Color = Color.Black
-            };
-            m_file.AllLayers.Add(l);
+            CreateLayer(arr[0], Color.Black);
         }
 
-        public int CreateLayer(string layerPath, Color color)
+        private Guid CreateLayer(string name, Color color)
         {
-            // 查询指定路径图层是否存在
-            Layer layer = FindLayerByFullPath(layerPath);
+            Guid id;
 
-            if (layer != null) return layer.Index;
+            // 从LayerMap中根据layerPath找到ID
+            if (LayerMap.TryGetValue(name, out id)) return id;
 
+            id = Guid.NewGuid();
+            LayerMap.Add(name, id);
+
+            Layer layer = new Layer
+            {
+                Name = name,
+                Color = color,
+                Id = id
+            };
+
+            m_file.AllLayers.Add(layer);
+
+            return id;
+        }
+
+        private Guid CreateChildLayer(string path, Color color, Guid parent)
+        {
+            Guid id;
+
+            // 从LayerMap中根据layerPath找到ID
+            if (LayerMap.TryGetValue(path, out id)) return id;
+
+            id = Guid.NewGuid();
+            LayerMap.Add(path, id);
+
+            string tmp = path.Replace(Layer.PathSeparator, "-");
+            string[] arr = tmp.Split('-');
+
+            Layer layer = new Layer
+            {
+                ParentLayerId = parent,
+                Name = arr[1],
+                Color = color,
+                Id = id
+            };
+
+            m_file.AllLayers.Add(layer);
+
+            return id;
+        }
+
+        public int GetLayer(string layerPath, Color color)
+        {
+            Guid id;
+
+            // 先根据图层路径执行子图层解决方案
             ChildLayerSolution(layerPath);
 
-            // Null不存在，需要创建
-            if (layerPath.Contains("::"))
+            // 从LayerMap中根据layerPath找到ID
+            if (!LayerMap.TryGetValue(layerPath, out id))
             {
-                // 这种情况需要先创建父图层，再创建子图层
-                string tmp = layerPath.Replace("::", "-");
-                string[] nameArr = tmp.Split('-');
-
-                Layer parent = FindLayerByFullPath(nameArr[0]);
-                if (parent == null) return 0;
-
-                Layer child = FindLayerByFullPath(layerPath);
-
-                if (child != null) return child.Index;
-
-                Layer l = new Layer
+                // 不存在，需要分情况创建
+                if (layerPath.Contains(Layer.PathSeparator))
                 {
-                    ParentLayerId = parent.Id,
-                    Name = nameArr[1],
-                    Color = color
-                };
-                m_layers.Add(l);
+                    // 这种情况需要先找到父图层，再创建子图层
+                    string tmp = layerPath.Replace(Layer.PathSeparator, "-");
+                    string[] arr = tmp.Split('-');
 
-                return m_layers.Count - 1;
-            }
-            else
-            {
-                // 这种情况直接创建图层
-                Layer la = FindLayerByFullPath(layerPath);
-                if (la != null) return la.Index;
+                    Guid parentID;
 
-                Layer l = new Layer
-                {
-                    Name = layerPath,
-                    Color = color
-                };
-                layer = l;
-                m_layers.Add(l);
-
-                return m_layers.Count - 1;
-            }
-        }
-
-        private Layer FindLayerByName(string name)
-        {
-            Layer ll = (from layer in m_file.AllLayers
-                        where layer.Name == name
-                        select layer).FirstOrDefault();
-            return ll;
-        }
-        private Layer FindLayerByFullPath(string path)
-        {
-            Layer ll = (from layer in m_file.AllLayers
-                        where layer.FullPath == path
-                        select layer).FirstOrDefault();
-            return ll;
-        }
-
-        public void Write(List<ObjectLayerInfo> G, List<int> att)
-        {
-            for (int i = 0; i < G.Count; i++)
-            {
-                GeometryBase g = GH_Convert.ToGeometryBase(G[i].Geometry);
-                ObjectAttributes attr = new ObjectAttributes
-                {
-                    LayerIndex = att[i]
-                };
-
-                if (g != null)
-                {
-                    switch (g.ObjectType)
+                    if (!LayerMap.TryGetValue(arr[0], out parentID))
                     {
-                        case ObjectType.Brep:
-                            m_file.Objects.AddBrep(g as Brep, attr);
-                            break;
-                        case ObjectType.Curve:
-                            m_file.Objects.AddCurve(g as Curve, attr);
-                            break;
-                        case ObjectType.Point:
-                            m_file.Objects.AddPoint((g as Rhino.Geometry.Point).Location, attr);
-                            break;
-                        case ObjectType.Surface:
-                            m_file.Objects.AddSurface(g as Surface, attr);
-                            break;
-                        case ObjectType.Mesh:
-                            m_file.Objects.AddMesh(g as Mesh, attr);
-                            break;
-                        case ObjectType.PointSet:
-                            m_file.Objects.AddPointCloud(g as PointCloud, attr); //This is a speculative entry
-                            break;
-                        default:
-                            break;
+                        return 0;// 这个情况应该不会出现
                     }
+
+                    id = CreateChildLayer(layerPath, color, parentID);
+                }
+                else id = CreateLayer(layerPath, color);
+            }
+
+            Layer layer = m_file.AllLayers.FindId(id);
+
+            if (layer == null) return 0;
+
+            return layer.Index;
+        }
+
+        public void Write()
+        {
+            foreach (KeyValuePair<ObjectAttributes, GeometryBase> item in ObjectMap)
+            {
+                var obj = item.Value;
+                var attr = item.Key;
+                if (obj == null) return;
+                switch (obj.ObjectType)
+                {
+                    case ObjectType.Brep:
+                        m_file.Objects.AddBrep(obj as Brep, attr);
+                        break;
+                    case ObjectType.Curve:
+                        m_file.Objects.AddCurve(obj as Curve, attr);
+                        break;
+                    case ObjectType.Point:
+                        m_file.Objects.AddPoint((obj as Rhino.Geometry.Point).Location, attr);
+                        break;
+                    case ObjectType.Surface:
+                        m_file.Objects.AddSurface(obj as Surface, attr);
+                        break;
+                    case ObjectType.Mesh:
+                        m_file.Objects.AddMesh(obj as Mesh, attr);
+                        break;
+                    default:
+                        break;
                 }
             }
             m_file.Write(m_path, 6);
             m_file.Dispose();
         }
     }
+
 }

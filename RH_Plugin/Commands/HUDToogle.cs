d6f302ba45b7208aa719c06af8dea4b5274eff7c
KaivnD <KaivnD@hotmail.com>
2019-12-25 00:50:34 +0800

    dev:开发中HUD

---
+++ RH_Plugin/Commands/HUDToogle.cs
@@ -0,0 +1,39 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Display;
+
+namespace Noah.Commands
+{
+    public class HUDToogle : Command
+    {
+        public override string EnglishName => "HUDToogle";
+        readonly static HUD hud = new HUD();
+
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            hud.Enabled = !hud.Enabled;
+            doc.Views.Redraw();
+
+            RhinoView.SetActive -= RhinoView_SetActive;
+            RhinoView.SetActive += RhinoView_SetActive;
+            
+            return Result.Success;
+        }
+
+        private void RhinoView_SetActive(object sender, ViewEventArgs e)
+        {
+            if (!hud.Enabled) return;
+            hud.Enabled = false;
+            hud.Enabled = true;
+            RhinoDoc.ActiveDoc.Views.Redraw();
+        }
+    }
+}
---
+++ RH_Plugin/UI/HUD.cs
@@ -0,0 +1,33 @@
+﻿using Rhino.Display;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Noah
+{
+    public class HUD: DisplayConduit
+    {
+        protected override void DrawForeground(DrawEventArgs e)
+        {
+            if (!Equals(e.RhinoDoc.Views.ActiveView.ActiveViewport.Name, e.Viewport.Name))
+            {
+                return;
+            }
+            var bounds = e.Viewport.Bounds;
+            int height = bounds.Width < 600 ? 100 : 200;
+            int width = bounds.Width < 600 ? 200 : 300;
+            Size size = new Size(width, height);
+            double margin = bounds.Width < 600 ? 10 : 20;
+            double padding = bounds.Width < 600 ? 5 : 10;
+            
+            var pt = new Point2d(bounds.Right - size.Width - margin, bounds.Top + margin);
+            var pt2 = new Point2d(pt.X + padding, pt.Y + padding);
+            e.Display.Draw2dRectangle(new Rectangle((int)pt.X, (int)pt.Y, size.Width, size.Height), Color.Black, 0, Color.FromArgb(30, Color.Black));
+            e.Display.Draw2dText("Hi Noah!", Color.White, pt2, false);
+        }
+    }
+}

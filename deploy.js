76ee2de464b9d1886e061e8d727e0052130ba0e9
KaivnD <KaivnD@hotmail.com>
2019-12-28 22:46:01 +0800

    CI:test deploy

---
+++ deploy.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const path = require("path");
+const OSS = require("ali-oss");
+
+const { IncomingWebhook } = require("@slack/webhook");
+
+const OSS_KEYID = process.env.OSS_ID;
+const OSS_KEYSECRET = process.env.OSS_SECRET;
+const SLACK_URL = process.env.SLACK_WEBHOOK;
+
+(async () => {
+  if (!OSS_KEYID || !OSS_KEYSECRET || !SLACK_URL) {
+    console.log(`Can't run deploy.`);
+    return;
+  }
+
+  console.log("Deploying to Noah server...");
+
+  const webhook = new IncomingWebhook(SLACK_URL);
+
+  const client = new OSS({
+    region: "oss-accelerate", // oss-cn-shanghai
+    accessKeyId: OSS_KEYID,
+    accessKeySecret: OSS_KEYSECRET,
+    bucket: "ncfz",
+    secure: true
+  });
+
+  try {
+    const buildDir = "./RH_Plugin/bin";
+
+    if (fs.existsSync(buildDir)) {
+      fs.readdirSync(buildDir).forEach(async file => {
+        const ext = path.extname(file);
+        const filePath = path.join(buildDir, file);
+        if (ext === ".rhi" || ext === ".macrhi") {
+          let stream = fs.createReadStream(filePath);
+          let result = await client.putStream(
+            `Noah/Plugin/Rhino/${file}`,
+            stream
+          );
+          console.log(
+            `[${result.res.statusCode}] Put ${result.name} to OSS is ${result.res.statusMessage}`
+          );
+        }
+      });
+    }
+
+    await webhook.send({
+      channel: "noah-client",
+      text: `Noah Plugin ${version} 已更新！`
+    });
+
+    console.log("Post build action is done !");
+  } catch (err) {
+    await webhook.send({
+      channel: "noah-client",
+      text: `${PLATFORM} ${version} deploy faild！`
+    });
+
+    console.log(err);
+  }
+})();
